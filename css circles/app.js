86063c51c0a86a8d24b8a95439cad92acaedae3b
Nana Kwame Nyarko <54988712+nknyarko@users.noreply.github.com>
2022-04-22 16:57:03 +0000

    basic DOM project with javascript

---
+++ css circles/app.js
@@ -0,0 +1,7 @@
+document.getElementByClassName("button-1").onclick=function (){
+    document.getElementById("red-circle").style.display="none";
+}
+
+document.getElementById("red-circle").onclick=function(){
+    document.getElementById("red-circle").style.display="none";
+}
